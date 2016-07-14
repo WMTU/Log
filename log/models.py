4098cef11296e34e292eba9de037e1a6226599e1
Joel Yauk <jbyauk@gmail.com>
2016-07-14 19:24:25 -0400

    Remove now unused helper function

--- log/models.py
+++ log/models.py
@@ -7,12 +7,6 @@ db = SQLAlchemy(app)
 # Bind the declarative base to an engine
 db.Model.metadata.reflect(db.engine, only = ['djlogs'])
 
-def dump_datetime(value):
-  # Deserialize datetime object into string form for JSON processing.
-  if value is None:
-    return None
-  return value.strftime("%Y-%m-%d") + 'T' + value.strftime("%H:%M:%S")
-
 # Define Song class for existing djlogs table
 class Song(db.Model):
   __table__ = db.Model.metadata.tables['djlogs']
