7870369044df754aafa375ad5c278e5d8999727e
Joel Yauk <jbyauk@gmail.com>
2016-07-14 20:54:39 -0400

    Add support for updating Icecast metadata

--- log/publishers.py
+++ log/publishers.py
@@ -8,18 +8,26 @@ network = LastFMNetwork(api_key       = app.config['LASTFM_API_KEY'],
                         username      = app.config['LASTFM_USERNAME'],
                         password_hash = md5(app.config['LASTFM_PASSWORD']))
 
+# Setup TuneIn parameters
+ti_params = {'partnerId':  app.config['TUNEIN_PARTNER_ID'],
+             'partnerKey': app.config['TUNEIN_PARTNER_KEY'],
+             'id':         app.config['TUNEIN_STATION_ID']}
+
 def publish(title, artist, album, timestamp):
   # Scrobble to Last.fm
   network.scrobble(artist=artist, title=title, timestamp=timestamp)
-  
-  # Setup TuneIn parameters
-  params = {'partnerId':  app.config['TUNEIN_PARTNER_ID'],
-            'partnerKey': app.config['TUNEIN_PARTNER_KEY'],
-            'id':         app.config['TUNEIN_STATION_ID'],
-            'title':      title,
-            'artist':     artist}
-  if (album):
-    params['album'] = album
-  get('http://air.radiotime.com/Playing.ashx', params = params)
 
-  # TODO: Icecast
+  # Update TuneIn metadata
+  ti_params['title']   = title
+  ti_params['artist']  = artist
+  if (album):
+    ti_params['album'] = album
+  get(app.config['TUNEIN_API_URI'], params = ti_params)
+
+  # Update Icecast metadata
+  uri = app.config['ICECAST_SERVER_URI'] + "admin/metadata"
+  ic_params = {'mode': 'updinfo',
+               'song': artist + " | " + title}
+  for m in app.config['ICECAST_MOUNTPOINTS']:
+    ic_params['mount'] = m
+    get(uri, params = ic_params, auth = (app.config['ICECAST_USERNAME'], app.config['ICECAST_PASSWORD']))
